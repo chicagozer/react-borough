ca47ecbd7eee0b56779a012f7dc5d6ea86bc1423
Jim Mittler <jim.mittler@xtime.com>
2016-09-29 21:33:52 -0700

    latest sync

--- src/rtdb/rtdb.service.tsx
+++ src/rtdb/rtdb.service.tsx
@@ -4,43 +4,32 @@ import {Borough} from '../components/borough';
 
 export class RtdbService {
 
-    private socket2: SocketIOClient.Socket;
+    private socket: SocketIOClient.Socket;
+    private url: string = 'https://rtdb.rheosoft.com';
 
-    private url2: string = 'https://rtdb.rheosoft.com';
-
-    private  boroughs: Observable<Borough[]>;
-
-
+    private boroughs: Observable<Borough[]>;
 
     constructor() {
 
+        this.socket = io(this.url);
 
-        console.log('creating socket2');
-        this.socket2 = io(this.url2);
-
-
-
-        this.socket2.on('connect', () => {
-            console.log('connected to socket2 now subscribing');
-
-            this.socket2.emit('subscribe', [{
+        this.socket.on('connect', () => {
+            this.socket.emit('subscribe', [{
                 view: '90e40254-d57c-4ce5-88b5-20034c9511ec'
-                //      ,ticket: response.json()
             }]);
 
         });
 
-        console.log('fetching boroughs');
-       this.boroughs = new Observable<Borough[]>((observer: any) => {
+        this.boroughs = new Observable<Borough[]>((observer: any) => {
 
-            this.socket2.on('90e40254-d57c-4ce5-88b5-20034c9511ec',
-                (data:any) => {
+            this.socket.on('90e40254-d57c-4ce5-88b5-20034c9511ec',
+                (data: any) => {
                     console.log('new borough data');
-                    observer.next(data.map((i:any) => new Borough(i[0], i[1].fvTotal, i[1].count)));
+                    observer.next(data.map((i: any) => new Borough(i[0], i[1].fvTotal, i[1].count)));
                 });
 
             return () => {
-                this.socket2.disconnect();
+                this.socket.disconnect();
             };
         });
 
@@ -48,7 +37,7 @@ export class RtdbService {
 
 
     public getBoroughs(): Observable<Borough[]> {
-    return this.boroughs;
+        return this.boroughs;
     }
 
 }
