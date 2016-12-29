b9b4b7441546287ea7ebc9860d60598aa514cc2b
Jim Mittler <jim.mittler@xtime.com>
2016-12-29 13:31:35 -0800

    latest

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
     // dependencies, which allows browsers to cache those libraries between builds.
     externals: {
         "react": "React",
-        "react-dom": "ReactDOM"
-    },
+        "react-dom": "ReactDOM",
+       "config": JSON.stringify(require('./package.json'))
+    }
 };
